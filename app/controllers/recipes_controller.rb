09a480274dcc7ae2a2c5d5d1e961b94779043aaf
GregT <taylor.gregory2@gmail.com>
2022-08-16 00:43:18 -0600

    commit tree f2755b78eccc0e62b57355408bd8e2280f0b89b6

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
 class RecipesController < ApplicationController
+    before_action :authorize
+    wrap_parameters format: []
+
+    def index
+        if session[:user_id]
+            recipes = Recipe.all
+            render json: recipes, include: :user
+        else
+        self.authorize
+        end
+    end
+
+    def create
+        recipe = Recipe.create!(recipe_params.merge(user_id: session[:user_id]))
+        render json: recipe, status: :created, include: :user
+    rescue ActiveRecord::RecordInvalid
+        render_unprocessable_entity
+
+    end
+
+    private
+
+    def render_unprocessable_entity
+        render json: {errors: ["Test"]}, status: :unprocessable_entity
+    end
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
+    def authorize
+        return render json: { errors: ["Not Authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
 end
