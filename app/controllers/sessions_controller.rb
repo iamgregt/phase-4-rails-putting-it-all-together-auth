3228dec54f047dae08d3079ba2b6af04752c0929
GregT <taylor.gregory2@gmail.com>
2022-08-16 00:43:35 -0600

    Done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,20 @@
 class SessionsController < ApplicationController
-
     def create
         user = User.find_by(username: params[:username])
-        # if !user
-        #     render json: {error: "Not found"}, status: :unauthorized
-        # else 
         if user&.authenticate(params[:password])
             session[:user_id] = user.id 
             render json: user, status: :created
         else  
         render json: {errors: ["Invalid Username"] }, status: :unauthorized
-        # end
+        end
+    end
+
+    def delete
+        if session[:user_id]
+        session.delete :user_id
+        head :no_content
+        else
+        render json: {errors: ["Not Logged In"]}, status: :unauthorized
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#delete"
+  get "/recipes", to: "recipes#index"
+  post "/recipes", to: "recipes#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
