fa246740c3d94bd1a6dba2652151852661ecf2fb
GregT <taylor.gregory2@gmail.com>
2022-08-15 18:55:55 -0600

    save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,15 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        if !user
-            render json: {error: "Not found"}, status: :unauthorized
-        else 
-        if user.authenticate(params[:password])
+        # if !user
+        #     render json: {error: "Not found"}, status: :unauthorized
+        # else 
+        if user&.authenticate(params[:password])
             session[:user_id] = user.id 
             render json: user, status: :created
         else  
-        render json: {error: {login: "Invalid username or password"}}, status: :unauthorized
-        end
+        render json: {errors: ["Invalid Username"] }, status: :unauthorized
+        # end
         end
     end
 end
